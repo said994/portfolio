dc6f4df1f8e0da9d0898f5879ce69aa05e4c5a89
said994 <saidyabrir@gmail.com>
2021-12-27 22:07:46 +0100

    fix links in the mobile version.

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const toggleBtn = document.querySelector(".toggle-btn");
 const navBar = document.querySelector(".nav__bar");
 const navBarList = document.querySelector(".nav__bar .list");
 const body = document.querySelector("body");
+const links = document.querySelectorAll(".nav__bar .list a");
 
 const handelResposiveNavBar = () => {
+  toggleNavBarClasses();
+  toggleBtn.classList.toggle("open");
+};
+
+const handelMobileLink = () => {
+  toggleNavBarClasses();
+};
+
+const toggleNavBarClasses = () => {
   navBarList.classList.toggle("hidden");
   body.classList.toggle("disable-scoll");
   navBar.classList.toggle("set-height-width");
-  toggleBtn.classList.toggle("open");
 };
+
+links.forEach((link) => {
+  link.addEventListener("click", handelMobileLink);
+});
 
 toggleBtn.addEventListener("click", handelResposiveNavBar);
